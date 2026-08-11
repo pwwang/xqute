6c947a5cd2ffe579286f7c15782450200e9a88c0
pwwang <pwwang@pwwang.com>
2026-08-11 09:55:11 -0700

    feat: add sanitize_mounts function to handle mount path sanitization and validation

---
+++ tests/test_utils.py
@@ -0,0 +1,99 @@
+import pytest
+
+from pathlib import Path
+from xqute.utils import sanitize_mounts
+
+
+async def test_sanitize_mounts_named_file():
+    sans_mounts, named_mounts_dict = await sanitize_mounts(
+        [f"thisfile={__file__}"],
+        "/mnt/disks",
+    )
+    assert len(sans_mounts) == 1
+    assert sans_mounts[0][0] == Path(__file__).parent
+    assert (
+        sans_mounts[0][1]
+        == Path("/mnt/disks/NAMED_MOUNTS") / "thisfile" / Path(__file__).parent.name
+    )
+    assert len(named_mounts_dict) == 1
+    assert (
+        str(named_mounts_dict["thisfile"])
+        == "/mnt/disks/NAMED_MOUNTS/thisfile/tests/test_utils.py"
+    )
+
+
+async def test_sanitize_mounts_named_dir():
+    sans_mounts, named_mounts_dict = await sanitize_mounts(
+        [f"thisdir={Path(__file__).parent}"],
+        "/mnt/disks",
+    )
+    assert len(sans_mounts) == 1
+    assert sans_mounts[0][0] == Path(__file__).parent
+    assert (
+        sans_mounts[0][1]
+        == Path("/mnt/disks/NAMED_MOUNTS") / "thisdir"
+    )
+    assert len(named_mounts_dict) == 1
+    assert (
+        str(named_mounts_dict["thisdir"])
+        == "/mnt/disks/NAMED_MOUNTS/thisdir"
+    )
+
+
+async def test_sanitize_mounts_single_mount():
+    sans_mounts, named_mounts_dict = await sanitize_mounts(
+        f"{Path(__file__).parent}:{Path(__file__).parent}",
+        "/mnt/disks",
+    )
+    assert len(sans_mounts) == 1
+    assert sans_mounts[0][0] == Path(__file__).parent
+    assert sans_mounts[0][1] == Path(__file__).parent
+    assert len(named_mounts_dict) == 0
+
+
+async def test_sanitize_mounts_invalid_mount():
+    with pytest.raises(ValueError):
+        await sanitize_mounts(
+            "invalid_mount_format",
+            "/mnt/disks",
+        )
+
+
+async def test_sanitize_mounts_duplicate_mounts():
+    sans_mounts, named_mounts_dict = await sanitize_mounts(
+        [
+            f"{Path(__file__).parent}:{Path(__file__).parent}",
+            f"{Path(__file__).parent}:{Path(__file__).parent}",
+        ],
+        "/mnt/disks",
+    )
+    assert len(sans_mounts) == 1
+    assert sans_mounts[0][0] == Path(__file__).parent
+    assert sans_mounts[0][1] == Path(__file__).parent
+    assert len(named_mounts_dict) == 0
+
+
+async def test_sanitize_mounts_duplicate_mounts_conflicting():
+    with pytest.raises(ValueError):
+        await sanitize_mounts(
+            [
+                f"{Path(__file__).parent}:{Path(__file__).parent}",
+                f"{Path(__file__).parent}/subdir:{Path(__file__).parent}",
+            ],
+            "/mnt/disks",
+        )
+
+
+async def test_sanitize_mounts_relative_mounts():
+    # longer one is silently removed because it is a subpath of the shorter one
+    sans_mounts, named_mounts_dict = await sanitize_mounts(
+        [
+            f"{Path(__file__).parent}:/mnt/disks/tests",
+            f"{Path(__file__).parent}/subdir:/mnt/disks/tests/subdir",
+        ],
+        "/mnt/disks",
+    )
+    assert len(sans_mounts) == 1
+    assert sans_mounts[0][0] == Path(__file__).parent
+    assert str(sans_mounts[0][1]) == "/mnt/disks/tests"
+    assert len(named_mounts_dict) == 0
--- xqute/utils.py
+++ xqute/utils.py
@@ -4,17 +4,123 @@ from __future__ import annotations
 
 import os
 import logging
-from typing import Union, Tuple, List
+import re
+from typing import Union, Tuple, List, Sequence
+from pathlib import Path
 
+from panpath import PanPath
 from rich.logging import RichHandler
 
 from .defaults import LOGGER_NAME, LOGGER_LEVEL
 
 CommandType = Union[str, Tuple[str], List[str]]
-
+NAMED_MOUNT_RE = re.compile(r"^[A-Za-z][A-Za-z0-9_]*=.+$")
 
 logger = logging.getLogger(LOGGER_NAME)
 logger.addHandler(RichHandler(show_path=False, omit_repeated_times=False))
 
 loglevel = os.getenv("XQUTE_LOG_LEVEL", LOGGER_LEVEL).upper()
 logger.setLevel(loglevel)
+
+
+async def sanitize_mounts(
+    mounts: Union[str, Sequence[str], None],
+    mount_root: str,
+    named_mounts: str = "NAMED_MOUNTS",
+) -> Tuple[List[Tuple[PanPath, Path]], dict[str, str]]:
+    """Sanitize the mounts
+
+    Args:
+        mounts: The mounts
+        mount_root: The root of the mounts
+
+    Returns:
+        A tuple of the sanitized mounts and a dictionary of named mounts
+    """
+    mounts = mounts or []
+    if not isinstance(mounts, Sequence) or isinstance(mounts, str):
+        mounts = [mounts]
+
+    sanitized_mounts = []
+    named_mounts_dict = {}
+    for mount in mounts:
+        if NAMED_MOUNT_RE.match(mount):
+            name, host_path = mount.split("=", 1)
+            host_path = PanPath(host_path)
+            if await host_path.a_is_file():
+                sanitized_mounts.append(
+                    (
+                        host_path.parent,
+                        Path(mount_root) / named_mounts / name / host_path.parent.name,
+                    )
+                )
+                named_mounts_dict[name] = str(
+                    Path(mount_root)
+                    / named_mounts
+                    / name
+                    / host_path.parent.name
+                    / host_path.name
+                )
+            else:
+                sanitized_mounts.append(
+                    (PanPath(host_path), Path(mount_root) / named_mounts / name)
+                )
+                named_mounts_dict[name] = str(Path(mount_root) / named_mounts / name)
+        else:
+            host_path, mount_path = mount.rpartition(":")[::2]
+            if not host_path or not mount_path:
+                raise ValueError(
+                    f"Invalid mount format: {mount}. Must be in the format of "
+                    "host_path:mount_path or name=host_path"
+                )
+            sanitized_mounts.append((PanPath(host_path), Path(mount_path)))
+
+    # Check if there are duplicate mount paths
+    # 1. if host paths are the same, remove one
+    mount_paths = [mount[1] for mount in sanitized_mounts]
+    to_remove = set()
+    for i, mount_path in enumerate(mount_paths):
+        for j, other_mount_path in enumerate(mount_paths):
+            if i >= j:
+                continue
+
+            if (
+                mount_path == other_mount_path
+                and sanitized_mounts[i][0] != sanitized_mounts[j][0]
+            ):
+                raise ValueError(
+                    f"Duplicate mount path: {mount_path} with different host paths. "
+                    f"Host paths: {sanitized_mounts[i][0]} and {sanitized_mounts[j][0]}"
+                )
+
+            if (
+                mount_path == other_mount_path
+                and sanitized_mounts[i][0] == sanitized_mounts[j][0]
+            ):
+                # remove the duplicate mount path
+                to_remove.add(j)
+
+    sanitized_mounts = [
+        mount for i, mount in enumerate(sanitized_mounts) if i not in to_remove
+    ]
+    # 2. if one mount path is a subpath of another, and the relative part is the same,
+    #    remove the longer one
+    to_remove = set()
+    for i, (host_path, mount_path) in enumerate(sanitized_mounts):
+        for j, (other_host_path, other_mount_path) in enumerate(sanitized_mounts):
+            if i == j:
+                continue
+
+            if mount_path.is_relative_to(other_mount_path):
+                mount_relative_part = mount_path.relative_to(other_mount_path)
+                host_relative_part = host_path.relative_to(other_host_path)
+                if mount_relative_part == host_relative_part:
+                    to_remove.add(i)
+
+    sanitized_mounts = [
+        mount for i, mount in enumerate(sanitized_mounts) if i not in to_remove
+    ]
+    # 3. if one mount path is a subpath of another, and the relative part is different,
+    #    keep both. Nothing to do here, just a note for clarity.
+
+    return sanitized_mounts, named_mounts_dict
